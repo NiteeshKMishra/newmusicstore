5ec2270346f9aa6dae483aa4fa63c0e552fe891e
iamniteeshmishra@gmail.com <nitish219@gmail.com>
2019-03-11 11:42:25 +0530

    Bug Fixes 2

--- public/js/client.js
+++ public/js/client.js
@@ -200,7 +200,7 @@ jQuery('#communicationform').on('submit', function (event) {
 //deletion of items
 
 var ditems = jQuery('[name=deletebutton]').attr('data');
-if (window.location.href === 'http://localhost:3000/checkout' && ditems !== undefined) {
+if (window.location.href === 'https://newmusicstore.herokuapp.com/checkout' && ditems !== undefined) {
   ditems = JSON.parse(ditems);
   for (i = 0; i < ditems.length; i++) {
     jQuery('#' + ditems[i]._id).on('click', function (event) {
